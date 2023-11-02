9342fe7870f72421c354ff163679184040e17b36
ciclom <MarcosLeonPortillo>
2023-11-02 14:20:14 +0100

    Buscaminas2 validar

--- Buscaminas/forms.py
+++ Buscaminas/forms.py
@@ -2,25 +2,19 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 class CreaTableroForm(forms.Form):
-    filas = forms.IntegerField(label='Filas',min_value=1, max_value=20, required= True, initial=2)
+    filas = forms.IntegerField(label='Filas', min_value=1, max_value=20, required=True, initial=2)
     columnas = forms.IntegerField(label='Columnas', min_value=1, max_value=15, required=True, initial=2)
     nMinas = forms.IntegerField(label='Minas', min_value=1, required=True, initial=2)
 
-def validar(self):
-    cleaned_data=super().clean()
-    filas = cleaned_data.get("filas")
-    columnas = cleaned_data.get("columnas")
-    nMinas= cleaned_data.get("minas") 
+    def clean(self):
+        cleaned_data = super().clean()
+        filas = cleaned_data.get("filas")
+        columnas = cleaned_data.get("columnas")
+        nMinas = cleaned_data.get("nMinas")
 
-    if nMinas>((columnas*filas)/2):
-        
-        raise ValidationError(" Valor de minas no válido ")
+        if nMinas > (columnas * filas) / 2:
+            raise ValidationError("El valor de minas no es válido")
 
-    return cleaned_data
-
-
-
-
-
+        return cleaned_data
 
 
